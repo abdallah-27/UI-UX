bd2ed4cf18a981ec9e1a2efb6273a22b4939d32c
abdallah96 <gueye.amadou1996@gmail.com>
2022-02-18 19:42:11 +0100

    Audio feature implemented

--- feature1.js
+++ feature1.js
@@ -1,4 +1,5 @@
 console.log("Feature 1 implemented");
 
-//git checkout
+//git checkout, to switch
 // git checkout master: Please switch to the latest version
+//master is always the stable version of the code
